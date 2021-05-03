e6345d9723e486daca3c8112f7edbcc40e554dfb
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-03 14:03:33 +0300

    Added login functionality

--- scripts/userServices.js
+++ scripts/userServices.js
@@ -1,9 +1,11 @@
 import { redirect } from './router.js';
+import { saveDataInStorage } from './authServices.js';
 
 const apiKey = "AIzaSyAkl19DDzE8x0ftC5SwNggOHSu3IAyh7jI";
 
 const endpoints = {
-    register: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`
+    register: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    login: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`
 }
 
 const request = {
@@ -16,24 +18,35 @@ const request = {
 
 }
 
+// TODO :: optimize it with function with switch
 const checkOnSubmit = {
 
     register: (emailElement, passwordElement, repPasswordElement) => {
-        
+
         let email = emailElement.value;
         let password = passwordElement.value;
         let repeatPassword = repPasswordElement.value;
 
 
         //TODO:: send notification
-        if(!email || !password || !repeatPassword) return false;
+        if (!email || !password || !repeatPassword) return false;
 
-        if(password !== repeatPassword) {
-           console.log(1);
+        if (password !== repeatPassword) {
+            console.log(1);
             passwordElement.value = '';
             repPasswordElement.value = '';
             return false;
         }
+
+        return true;
+    },
+
+    login: (emailElement, passwordElement) => {
+
+        let email = emailElement.value;
+        let password = passwordElement.value;
+
+        if (!email || !password) return false;
 
         return true;
     }
@@ -45,14 +58,44 @@ export async function register() {
     let passwordElement = document.querySelector('.registerPassword');
     let repeatPasswordElement = document.querySelector('.repeatPassword');
 
-    if(!checkOnSubmit.register(emailElement, passwordElement, repeatPasswordElement)) return;
+    if (!checkOnSubmit.register(emailElement, passwordElement, repeatPasswordElement)) return;
 
-    let body = { email: emailElement.value, password:passwordElement.value }
+    let body = { email: emailElement.value, password: passwordElement.value }
 
     let response = await request.post(endpoints.register, body);
 
-    if(response.ok) redirect('/login');
-    
+    if (response.ok) redirect('/login');
+
     //TODO :: else error notification!
 
+}
+
+export async function login() {
+
+    let emailElement = document.querySelector('.loginEmail');
+    let passwordElement = document.querySelector('.loginPassword');
+
+    if (!checkOnSubmit.login(emailElement, passwordElement)) return;
+
+    let body = { email: emailElement.value, password: passwordElement.value }
+
+    let response = await request.post(endpoints.login, body);
+
+    let userData = { isLogged: false }
+
+    if (!response.ok) {
+
+        saveDataInStorage('userData', userData);
+        // TODO : show error notification
+        return;
+
+    } else {
+
+        let data = await response.json();
+        userData.isLogged = true;
+        let userId = data.localId;
+        saveDataInStorage('userData', { ...userData, userId });
+        redirect('/');
+
+    }
 }
--- templates/login.js
+++ templates/login.js
@@ -1,9 +1,10 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 import  headerTemplate from '../templates/partials/header.js';
+import { login } from '../scripts/userServices.js';
 
 export default ( {
     isLogged
-}) => html`${headerTemplate()}
+}) => html`${headerTemplate(isLogged)}
 <div class="login-page flex flex--column">
           <div class="login-page__header">
             <h1>Login</h1>
@@ -14,7 +15,7 @@ export default ( {
               <input type="email" id="loginEmail" class="loginEmail" name="email">
               <label for="loginPassword"><h5>Password:</h5></label>
               <input type="password" id="loginPassword" class="loginPassword" name="password">
-              <a href="/" class="btn btn--primary navLink">Log in</a>
+              <button class="btn btn--primary" type="submit" @click=${login}>Login</button>
           </form>
           </div>
           <div class="footer">Copyright@ 2021 Designed by Ivan Dimitrov</div>
