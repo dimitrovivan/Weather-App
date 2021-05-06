5411b1d548bb9f2a73540a343d450d247f568be8
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-06 10:27:49 +0300

    Moved back btn to be in btn comp for reusability

--- templates/notFound.js
+++ templates/notFound.js
@@ -7,7 +7,7 @@ export default ({
 
 }) => html`${headerTemplate(isLogged)}
 <div class="page-404">
-    <button class="btn btn--primary" @click=${backOnPush}>Back</button>
+    <button class="btn btn--primary btn--back" @click=${backOnPush}>Back</button>
     <h1 class="page-404__heading text-centered">Invalid city or country name</h1>
     <div class="footer">Copyright@ 2021 Designed by Ivan Dimitrov</div>
 </div>
